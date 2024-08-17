65fb07183bce5ae0d6561de42b14c3bb6dabf242
Agni Karmazi <karm_agn@yahoo.gr>
2024-08-18 02:52:59 +0300

    DC-0004 fix wrong added characters at project No 1, my approach

---
+++ Udemy_20_projects/2_jumping_letters/app.js
@@ -0,0 +1,8 @@
+const letters = document.querySelectorAll(".letters span");
+
+letters.forEach((letter)=>{
+
+    letter.addEventListener("click",(e)=>{
+        e.target.classList.add("active");
+    });
+});
